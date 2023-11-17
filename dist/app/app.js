301f0d29aff68bb63a397a51e546bbe490a9caa2
Sadman600 <sadmansakib600@gmail.com>
2023-11-17 12:27:49 +0600

    create some route

--- dist/app/app.js
+++ dist/app/app.js
@@ -5,7 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
+app.use(express_1.default.json());
+const middleWare = (req, res, next) => {
+    console.log(req.url, req.path);
+    next();
+};
+app.get("/", middleWare, (req, res) => {
+    //   console.log(req.query);
     res.send("Hello World!");
+});
+app.get("/:userId", (req, res) => {
+    console.log(req.params);
+    res.send("Hello user");
+});
+app.post("/user", (req, res) => {
+    console.log(req.body);
+    res.send("data send sussess");
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,27 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.use(express.json());
+
+const middleWare = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.path);
+  next();
+};
+app.get("/", middleWare, (req: Request, res: Response) => {
+  //   console.log(req.query);
+
   res.send("Hello World!");
+});
+app.get("/:userId", (req: Request, res: Response) => {
+  console.log(req.params);
+  res.send("Hello user");
+});
+
+app.post("/user", (req: Request, res: Response) => {
+  console.log(req.body);
+
+  res.send("data send sussess");
 });
 
 export default app;
